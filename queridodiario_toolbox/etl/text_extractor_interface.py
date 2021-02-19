7ac5a5b612ba49fc09b2b377add5f64c7f948c55
José Guilherme Vanz <jvanz@jvanz.com>
2021-02-19 15:44:32 -0300

    Text extractor interface.
    The text extractor interface goal is to abstract for the user all the
    logic necessary to extract text from a given gazette. The interface has
    methods to extract the text and metadata of the given gazette them back
    to the disk in files. It also has the methods to load these extract data
    into memory to allow the user manipulation.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

---
+++ queridodiario_toolbox/etl/text_extractor_interface.py
@@ -0,0 +1,35 @@
+import abc
+
+class TextExtractor(abc.ABC):
+    """
+    Text extractor interface. This interface define the methods that all text
+    extractor should have in order to properly work with the library
+    """
+
+    @abc.abstractmethod
+    def extract_text(self, gazette):
+        """
+        Extract the text from the given gazette.
+        """
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def load_content(self, gazette):
+        """
+        Load into memory the text from the given gazette.
+        """
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def extract_metadata(self, gazette):
+        """
+        Extract the given gazette file metadata
+        """
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def load_metadata(self, gazette):
+        """
+        Load into memory the given gazette file metadata
+        """
+        raise NotImplemented()
